685a354de676fbca4cb5313d38c4d4840a0a645b
PUESTO_01\Fabri <evelynat21@gmail.com>
2023-08-07 13:02:40 -0300

    estructura basica de la pagina de detalles del producto

--- Frontend/secotool-app/src/components/carousel/Carousel.jsx
+++ Frontend/secotool-app/src/components/carousel/Carousel.jsx
@@ -3,6 +3,12 @@ import styles from './Carousel.module.css';
 
 function Carousel ({ imagenes }) {
     const [imagenActual, setImagenActual] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+    const indicadorImagenes = `${imagenActual + 1}/${imagenes.length}`;
+
+    const toggleShowMore = () => {
+        setShowMore(!showMore);
+    };
 
     if(!Array.isArray(imagenes) || imagenes.length === 0)
     return;
@@ -12,12 +18,15 @@ function Carousel ({ imagenes }) {
     }
 
     const anteriorImagen = () => {
-        setImagenActual(imagenActual === imagenes.length - 1 ? 0 : imagenActual - 1)
+        setImagenActual(imagenActual === 0 ? imagenes.length - 1 : imagenActual - 1)
     }
 
   return (
     <>
-    <div className={styles.containerCarousel}>
+    <div className={styles.containerCarouselMobile}>
+        <div className={styles.indicadorImg}>
+            {indicadorImagenes}
+        </div>
         {imagenes.map((imagen, index) => {
             return (
                 // eslint-disable-next-line react/jsx-key
@@ -25,7 +34,7 @@ function Carousel ({ imagenes }) {
                     ? `${styles.slide} ${styles.active}`
                     : styles.slide}>
                     {imagenActual===index && (
-                        <img className={styles.imgProduct} key={index} src={imagen} alt="" />
+                        <img key={index} className={styles.imgProduct} src={imagen} alt="" />
                     )}
                 </div>
             );
@@ -34,6 +43,32 @@ function Carousel ({ imagenes }) {
         <button onClick={anteriorImagen}><i className="fa-regular fa-chevron-left"></i></button>
         <button onClick={siguienteImagen}><i className="fa-regular fa-chevron-right"></i></button>
         </div>
+    </div>
+    <div className={styles.containerCarousel}>
+        <div className={styles.carouselGrid}>
+            <div className={styles.leftColumn}>
+                <img className={styles.imgProduct} src={imagenes[0]} alt="" />
+            </div>
+            <div className={styles.rightColumn}>
+                {imagenes.slice(1,5).map((imagen, index) => (
+                <div key={index}>
+                    <img className={styles.imgProduct} src={imagen} alt="" />
+                </div>
+                ))}
+            </div>
+        </div>
+        {showMore && (
+            <div className={styles.moreImagesContainer}>
+                {imagenes.slice(5).map((imagen, index) => (
+                    <div key={index}>
+                        <img className={styles.imgProduct} src={imagen} alt="" />
+                    </div>
+                ))}
+            </div>
+        )}
+        <button className='button-transparent' onClick={toggleShowMore}>
+            {showMore ? 'Ver menos' : 'Ver más'}
+        </button>
     </div>
     </>
   );
