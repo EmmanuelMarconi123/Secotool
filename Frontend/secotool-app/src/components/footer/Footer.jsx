7179919a6834be67d60c38a22da16658de88530c
darioplazaleon <darioplaza02@hotmail.com>
2023-08-09 20:45:37 -0300

    Footer hecho

--- Frontend/secotool-app/src/components/footer/Footer.jsx
+++ Frontend/secotool-app/src/components/footer/Footer.jsx
@@ -1,11 +1,16 @@
+import "../footer/Footer.module.css";
+
 function Footer() {
-    /*aqui va todo referido al footer*/
-    return(
-        <>
-        <footer className="spacing-grid">
-            <h1>Footer</h1>
-        </footer>
-        </>
-    )
+  /*aqui va todo referido al footer*/
+  return (
+    <>
+      <footer className="spacing-grid">
+        <img src="src\assets\img\logo-blanco-footer.png" alt=""/>
+          <p style={{ margin: 0 }}>
+            ©2023 SecoTool
+          </p>
+      </footer>
+    </>
+  );
 }
 export default Footer;
