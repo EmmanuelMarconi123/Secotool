0384523098b3308b6ce8940b48222439034c58c7
Emmanuel <emmanuelmarconi19@gmail.com>
2023-08-10 10:24:15 +1000

    subiendo codigo emma

--- Frontend/secotool-app/src/components/card/CardProduct.jsx
+++ Frontend/secotool-app/src/components/card/CardProduct.jsx
@@ -1,4 +1,39 @@
-function CardProduct() {
-    /*Aqui va la maqueta del card del producto, el cual se usara en el listado*/
-}
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid, IconButton } from "@mui/material";
+import styles from "../card/CardProduct.module.css";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+function CardProduct({ nombre, id, descripcion }) {
+    return (
+    <Grid container className={styles.container}>
+      <Grid item xs={11} className={styles.img}>
+        <img
+          className={styles.img}
+          src="src/assets/recursos/photo-1689600570529-0e87f50e4687.avif"
+          alt=""
+        />
+      </Grid>
+      <Grid className={styles.textoCard} item xs={11}>
+        <Grid className={styles.card} container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="body1" color="text.secondary">
+              {nombre}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {id}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" size="small">
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <AddShoppingCartIcon />
+              </IconButton>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+    }
 export default CardProduct;
--- Frontend/secotool-app/src/components/routes/Home.jsx
+++ Frontend/secotool-app/src/components/routes/Home.jsx
@@ -1,9 +1,101 @@
-function Home() {
-//Aqui va todo referido al home(no admin) de la app.
-return (
-    <>
-    <h1>Home</h1>
-    </>
-)
-}
+import { Typography } from "@mui/material";
+import Form from "../form/FormBusqueda";
+import styles from "../routes/home.module.css";
+import Cards from "../card/CardProduct";
+
+
+const Home = () => {
+  const herramienasDeAlquiler = [
+    {
+      id: 1,
+      nombre: "martillo",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 2,
+      nombre: "taladro",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 3,
+      nombre: "tornillo",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 4,
+      nombre: "pala",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 5,
+      nombre: "retroescabadora",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 6,
+      nombre: "cortadora",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 7,
+      nombre: "maderas",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 8,
+      nombre: "fierros",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 9,
+      nombre: "grua",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+    {
+      id: 10,
+      nombre: "soldadora",
+      descripcion: "lakjslkajsdlkj alskhalksdj",
+    },
+  ];
+
+  // Función para barajar un array utilizando el algoritmo de Fisher-Yates
+  function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  }
+
+  const shuffledHerramientas = shuffleArray(herramienasDeAlquiler); //herramienta para hacer aleatoria la forma en que se renderizan las cards
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.headerSpacer}></div>
+      {/* Div para empujar el contenido hacia abajo */}
+      <div className={styles.container}>
+        <Typography color="primary" variant="h6" className={styles.titulo}>
+          Buscador de Herramientas
+        </Typography>
+        <Form />
+      </div>
+      <div className={styles.contenedorCards}>
+        {shuffledHerramientas.map((card) => (
+          <div key={card.id} className={styles.card}>
+            <Cards
+              id={card.id}
+              nombre={card.nombre}
+              descripcion={card.descripcion}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Home;
