3b6fe46b411814ae81f5195c3648db1234405441
francroce <francocrocee7@gmail.com>
2023-08-19 13:22:24 -0300

    subiendo nuevo header admin

--- Frontend/secotool-app/src/components/routes/homeAdmin/HomeAdmin.jsx
+++ Frontend/secotool-app/src/components/routes/homeAdmin/HomeAdmin.jsx
@@ -4,109 +4,99 @@ import { useEffect, useState } from "react";
 import Pagination from "../../pagination/Pagination";
 
 const HomeAdmin = () => {
-  const ArrProducts = [
-    {
-      id: "0001",
-      title:
-        "Taladro Percutor atornillador Bosch Professional GSB 550 RE Caja de cartón - Azul - 220V",
-    },
-    {
-      id: "0002",
-      title: "Martillo Inoxidable Professional Hetcher",
-    },
-    {
-      id: "0003",
-      title: "Pinza punzadora profesional Smith",
-    },
-    {
-      id: "0004",
-      title: "Sierra sensitiva Bosch ATB 530 V2 - Negra - 220V",
-    },
-    {
-      id: "0005",
-      title: "Destornillador electrico Hiui alta durabilidad - Rojo",
-    },
-    {
-      id: "0006",
-      title: "Nivel Laser Reifelmenz 10 metros",
-    },
-    {
-      id: "0007",
-      title: "Amoladora Steel 5 discos con soporte automatico - 220V",
-    },
-    {
-      id: "0008",
-      title: "Gafas de proteccion Sahira de alta resistencia trasparentes",
-    },
-    {
-      id: "0009",
-      title: "Sierra Cortadora Sensitiva Metal Yosemite 2100w 355mm 14puLG",
-    },
-    {
-      id: "0010",
-      title: "Caja De Herramientas Con 9 Piezas Truper Pinza Alicate",
-    },
-    {
-      id: "0011",
-      title: "Set De Chapista Black Jack X 7 Pcs 3 Martillos 4 Aguantadore",
-    },
-    {
-      id: "0012",
-      title: "Pinza Pelacables Automatico Frontal Bremen Corta Cable 7725",
-    },
-    {
-      id: "0013",
-      title: "Taladro Atornillador Percutor + 2 Baterias Gp By Lusqtoff",
-    },
-    {
-      id: "0014",
-      title: "Mini Motosierra Electrosierra Recargable Batería Inalámbrica",
-    },
-    {
-      id: "0015",
-      title: "Destornillador electrico Hiui alta durabilidad - Rojo",
-    },
-    {
-      id: "0016",
-      title: "Juego De Llaves Combinadas Fija Estriada 12 Piezas 6 A 22 Mm",
-    },
-    {
-      id: "0017",
-      title: "Llave Caño Stilson Bahco 142 426 Mm Apertura 65 Mm",
-    },
-    {
-      id: "0018",
-      title: "Lijadora Rotoorbital 300w 125mm 5 PuLG Vel Variable + Lijas",
-    },
-    {
-      id: "0019",
-      title: "Pinza Sacabocado Profesional Para Perforar Cuero Plástico",
-    },
-    {
-      id: "0020",
-      title: "Caja De Herramientas Con 9 Piezas Truper Pinza Alicate",
-    },
-  ];
+  // const ArrProducts = [
+  //   {
+  //     id: "0001",
+  //     title:
+  //       "Taladro Percutor atornillador Bosch Professional GSB 550 RE Caja de cartón - Azul - 220V",
+  //   },
+  //   {
+  //     id: "0002",
+  //     title: "Martillo Inoxidable Professional Hetcher",
+  //   },
+  //   {
+  //     id: "0003",
+  //     title: "Pinza punzadora profesional Smith",
+  //   },
+  //   {
+  //     id: "0004",
+  //     title: "Sierra sensitiva Bosch ATB 530 V2 - Negra - 220V",
+  //   },
+  //   {
+  //     id: "0005",
+  //     title: "Destornillador electrico Hiui alta durabilidad - Rojo",
+  //   },
+  //   {
+  //     id: "0006",
+  //     title: "Nivel Laser Reifelmenz 10 metros",
+  //   },
+  //   {
+  //     id: "0007",
+  //     title: "Amoladora Steel 5 discos con soporte automatico - 220V",
+  //   },
+  //   {
+  //     id: "0008",
+  //     title: "Gafas de proteccion Sahira de alta resistencia trasparentes",
+  //   },
+  //   {
+  //     id: "0009",
+  //     title: "Sierra Cortadora Sensitiva Metal Yosemite 2100w 355mm 14puLG",
+  //   },
+  //   {
+  //     id: "0010",
+  //     title: "Caja De Herramientas Con 9 Piezas Truper Pinza Alicate",
+  //   },
+  //   {
+  //     id: "0011",
+  //     title: "Set De Chapista Black Jack X 7 Pcs 3 Martillos 4 Aguantadore",
+  //   },
+  //   {
+  //     id: "0012",
+  //     title: "Pinza Pelacables Automatico Frontal Bremen Corta Cable 7725",
+  //   },
+  //   {
+  //     id: "0013",
+  //     title: "Taladro Atornillador Percutor + 2 Baterias Gp By Lusqtoff",
+  //   },
+  //   {
+  //     id: "0014",
+  //     title: "Mini Motosierra Electrosierra Recargable Batería Inalámbrica",
+  //   },
+  //   {
+  //     id: "0015",
+  //     title: "Destornillador electrico Hiui alta durabilidad - Rojo",
+  //   },
+  //   {
+  //     id: "0016",
+  //     title: "Juego De Llaves Combinadas Fija Estriada 12 Piezas 6 A 22 Mm",
+  //   },
+  //   {
+  //     id: "0017",
+  //     title: "Llave Caño Stilson Bahco 142 426 Mm Apertura 65 Mm",
+  //   },
+  //   {
+  //     id: "0018",
+  //     title: "Lijadora Rotoorbital 300w 125mm 5 PuLG Vel Variable + Lijas",
+  //   },
+  //   {
+  //     id: "0019",
+  //     title: "Pinza Sacabocado Profesional Para Perforar Cuero Plástico",
+  //   },
+  //   {
+  //     id: "0020",
+  //     title: "Caja De Herramientas Con 9 Piezas Truper Pinza Alicate",
+  //   },
+  // ];
 
   //-------------- CONFIGURACION DE LA PAGINACION -------------------->
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
-
-  const lastPostIndex = currentPage * itemsPerPage;
-  const fistPostIndex = lastPostIndex - itemsPerPage;
-  const currentPost = ArrProducts.slice(fistPostIndex, lastPostIndex);
+  const [products, setProducts] = useState([]);
+  const [currentPost, setCurrentPost] = useState([]);
 
   const [matches, setMatches] = useState(
     window.matchMedia("(min-width: 1024px)").matches
   );
-
-  const [products, setProducts] = useState(ArrProducts);
-
-  async function getData() {
-    const response = await (await fetch("URL TO FETCH")).json();
-    setProducts(response);
-  }
 
   function deleteItem(id) {
     console.log("Se ha borrado el item con id " + id);
@@ -115,9 +105,17 @@ const HomeAdmin = () => {
     console.log(products);
   }
 
-  //   async function deleteItem(id) {
-  //     const response = await (await fetch("URL TO FETCH")).json()
-  //     setProducts(response);
+  // async function deleteItem(id) {
+  // try {
+  //   const response = await fetch("http://localhost:8080/v1/api/products/{id}");
+  //   if (response.ok) {
+  //     console.log(`Se ha borrado el item con id ${id} correctamente`);
+  //   } else {
+  //     throw new Error("Error en la solicitud");
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
   //  }
 
   useEffect(() => {
@@ -127,8 +125,28 @@ const HomeAdmin = () => {
   }, []);
 
   useEffect(() => {
-    getData();
+    const fetchProductsAdmin = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/v1/api/products");
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data); //Borrar este console.log, mas tarde\
+          setProducts(data);
+        } else {
+          throw new Error("Error en la solicitud");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProductsAdmin();
   }, []);
+
+  useEffect(() => {
+    const lastPostIndex = currentPage * 10;
+    const fistPostIndex = lastPostIndex - 10;
+    setCurrentPost(products.slice(fistPostIndex, lastPostIndex));
+  }, [currentPage, products]);
 
   return (
     <div>
@@ -146,18 +164,23 @@ const HomeAdmin = () => {
                 <span>Nombre</span>
                 <span>Acciones</span>
               </div>
-              {products &&
+              {products.length > 0 ? (
                 currentPost.map((product) => (
                   <AdminProductCard
                     key={product.id}
                     deleteItem={() => deleteItem(product.id)}
                     id={product.id}
-                    title={product.title}
+                    title={product.name}
                   />
-                ))}
+                ))
+              ) : (
+                <span className={styles.noProducstMessage}>
+                  No se encontraron resultados
+                </span>
+              )}
               <Pagination
-                totalPosts={ArrProducts.length}
-                itemsPerPage={itemsPerPage}
+                totalPosts={products.length}
+                itemsPerPage={10}
                 setCurrentPage={setCurrentPage}
                 currentPage={currentPage}
               />
