2ad890f9d26dc499ae9ad211c7ce150fc53ff1d8
darioplazaleon <darioplaza02@hotmail.com>
2023-08-22 01:02:10 -0300

    Fetch hecho add product y delete product

--- Frontend/secotool-app/src/components/routes/homeAdmin/HomeAdmin.jsx
+++ Frontend/secotool-app/src/components/routes/homeAdmin/HomeAdmin.jsx
@@ -4,9 +4,141 @@ import { useEffect, useState } from "react";
 import Pagination from "../../pagination/Pagination";
 import { ButtonToolbar, Button, Uploader, TagPicker } from "rsuite";
 import Modal from "rsuite/Modal";
+import { Alert, Snackbar } from "@mui/material";
+import axios from "axios";
 
 const HomeAdmin = () => {
-  //------------------------------CONFIG MULTICASCADA---------------------->
+  //------------------------------ CONFIG MODALS--------------->
+  const [open, setOpen] = useState(false); //NEW PRODUCT MODAL
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  const [openEp, setOpenEp] = useState(false); // EDIT PRODUCT MODAL
+  const [selectedProduct, setSelectedProduct] = useState([]);
+  const handleOpenEp = (product) => {
+    setSelectedProduct(product);
+    setOpenEp(true);
+  };
+  const handleCloseEp = () => setOpenEp(false);
+  //--------------------------------NEW PRODUCT---------------------->
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [uploadedImages, setUploadedImages] = useState([]); // Estado para las imágenes cargadas
+  
+
+  const handleImageChange = (e) => {
+    const selectedImages = Array.from(e.target.files); // Convertir FileList a un array
+    setUploadedImages(selectedImages);
+  };
+
+
+  const handleNewProductSubmit = async (e) => {
+    e.preventDefault();
+
+    const dataC = { name, category, description, price };
+
+    const json = JSON.stringify(dataC)
+    const blob = new Blob([json], {
+      type: 'application/json'
+    })
+
+    
+    const formData = new FormData();
+    formData.append("data", blob)
+    uploadedImages.forEach((image) => {
+      formData.append("images", image);
+    });
+
+    console.log(dataC);
+    console.log(formData)
+
+    axios({
+      method: "post",
+      url: "http://localhost:8080/v1/api/products",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
+
+  //---------------------------------DELETE PRODUCT------------------------------->
+
+  const [alertOpen, setAlertOpen] = useState(false);
+  const showDeleteSuccessAlert = () => {
+    setAlertOpen(true);
+  };
+  async function deleteProduct(productId) {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/v1/api/products/${productId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (response.ok) {
+        console.log(`Producto con ID ${productId} eliminado correctamente`);
+        // Aquí podrías actualizar tu lista de productos eliminando el producto con el ID correspondiente
+        setProducts(products.filter((product) => product.id !== productId));
+        showDeleteSuccessAlert(); // Muestra la alerta de éxito
+      } else {
+        throw new Error("Error al eliminar el producto");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //---------------------------------------EDIT PRODUCT-------------------->
+  
+  const [editedName, setEditedName] = useState(selectedProduct.name);
+  const [editedDescription, setEditedDescription] = useState(selectedProduct.description);
+
+  const handleNameChange = (event) => {
+    setEditedName(event.target.value);
+  };
+  
+  const handleDescriptionChange = (event) => {
+    setEditedDescription(event.target.value);
+  };
+
+  async function editProduct(productId) {
+    try {
+      const response = await fetch(`http://localhost:8080/v1/api/products/${productId}`, {
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({
+          name: editedName,
+          description: editedDescription,
+        }),
+      });
+  
+      if (response.ok) {
+        console.log(`Producto con ID ${productId} editado correctamente`);
+        // Cierra el modal de edición
+        handleCloseEp();
+      } else {
+        throw new Error('Error al editar el producto');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //------------------------------DATOS---------------------->
   const categories = [
     "Electrónica",
     "Ropa",
@@ -25,19 +157,6 @@ const HomeAdmin = () => {
     "Tecnología de última generación",
   ].map((item) => ({ label: item, value: item }));
 
-  //------------------------------ CONFIG MODALS--------------->
-  const [open, setOpen] = useState(false); //NEW PRODUCT MODAL
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const [openEp, setOpenEp] = useState(false); // EDIT PRODUCT MODAL
-  const [selectedProduct, setSelectedProduct] = useState([]);
-  const handleOpenEp = (product) => {
-    setSelectedProduct(product)
-    setOpenEp(true);
-  }
-  const handleCloseEp = () => setOpenEp(false);
-
 
   //-------------- CONFIGURACION DE LA PAGINACION -------------------->
 
@@ -48,26 +167,6 @@ const HomeAdmin = () => {
   const [matches, setMatches] = useState(
     window.matchMedia("(min-width: 1024px)").matches
   );
-
-  function deleteItem(id) {
-    console.log("Se ha borrado el item con id " + id);
-    const newProducts = products.filter((product) => product.id !== id);
-    setProducts(newProducts);
-    console.log(products);
-  }
-
-  // async function deleteItem(id) {
-  // try {
-  //   const response = await fetch("http://localhost:8080/v1/api/products/{id}");
-  //   if (response.ok) {
-  //     console.log(`Se ha borrado el item con id ${id} correctamente`);
-  //   } else {
-  //     throw new Error("Error en la solicitud");
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  // }
-  //  }
 
   useEffect(() => {
     window
@@ -81,7 +180,6 @@ const HomeAdmin = () => {
         const response = await fetch("http://localhost:8080/v1/api/products");
         if (response.ok) {
           const data = await response.json();
-          console.log(data); //Borrar este console.log, mas tarde\
           setProducts(data);
         } else {
           throw new Error("Error en la solicitud");
@@ -125,7 +223,7 @@ const HomeAdmin = () => {
                 currentPost.map((product) => (
                   <AdminProductCard
                     key={product.id}
-                    deleteItem={() => deleteItem(product.id)}
+                    deleteItem={() => deleteProduct(product.id)}
                     id={product.id}
                     title={product.name}
                     editItem={() => handleOpenEp(product)}
@@ -158,6 +256,16 @@ const HomeAdmin = () => {
           Por favor ingrese desde un dispositivo más grande
         </span>
       )}
+      {/* -----------------------DELETE ALERT---------------------> */}
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={3000} // Duración en milisegundos
+        onClose={() => setAlertOpen(false)}
+      >
+        <Alert onClose={() => setAlertOpen(false)} severity="success">
+          Producto eliminado correctamente.
+        </Alert>
+      </Snackbar>
       {/* --------------------------NUEVO PRODUCTO MODAL--------------------------------> */}
       <Modal size="md" open={open} onClose={handleClose} overflow={false}>
         <Modal.Header>
@@ -167,22 +275,33 @@ const HomeAdmin = () => {
         </Modal.Header>
         <Modal.Body className={styles.containerModal}>
           <div className={styles.centeredForm}>
-            <form className={styles.formNewProduct} action="">
+            <form
+              className={styles.formNewProduct}
+              onSubmit={handleNewProductSubmit}
+            >
               <label htmlFor="">
                 Nombre del producto
-                <input type="text" />
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </label>
               <label htmlFor="">
                 Descripcion
                 <textarea
                   cols="30"
                   rows="10"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                   style={{ height: 120, width: 640 }}
                 ></textarea>
               </label>
               <label htmlFor="">
                 Categorias
-                <TagPicker style={{ width: 640 }} data={categories} />
+                <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
               </label>
               <label htmlFor="">
                 Caracteristicas
@@ -190,26 +309,16 @@ const HomeAdmin = () => {
               </label>
               <label htmlFor="">
                 Precio
-                <input type="number" />
+                <input
+                  type="text"
+                  name="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
               </label>
               <label htmlFor="">
                 Imagenes
-                <Uploader draggable>
-                  <div
-                    style={{
-                      height: 54,
-                      width: 640,
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      gap: 10,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <i className="fa-solid fa-cloud-arrow-up"></i>
-                    <span>Subir imagen</span>
-                  </div>
-                </Uploader>
+                <input type="file" onChange={handleImageChange} accept="image/*" multiple  />
               </label>
               <div className={styles.labelSeparator}></div>
               <button>Agregar Producto</button>
@@ -227,16 +336,17 @@ const HomeAdmin = () => {
         <Modal.Body className={styles.containerModal}>
           <div className={styles.centeredForm}>
             <form className={styles.formNewProduct} action="">
-              <label htmlFor="">
+              <label htmlFor="editProductName">
                 Nombre del producto
-                <input type="text" defaultValue={selectedProduct.name}/>
+                <input type="text" defaultValue={selectedProduct.name} onChange={handleNameChange}/>
               </label>
-              <label htmlFor="">
+              <label htmlFor="editDescription">
                 Descripcion
                 <textarea
                   cols="30"
                   rows="10"
                   defaultValue={selectedProduct.description}
+                  onChange={handleDescriptionChange}
                   style={{ height: 120, width: 640 }}
                 ></textarea>
               </label>
@@ -272,7 +382,7 @@ const HomeAdmin = () => {
                 </Uploader>
               </label>
               <div className={styles.labelSeparator}></div>
-              <button>Agregar Producto</button>
+              <button onClick={() => editProduct(selectedProduct.id)}>Guardar Cambios</button>
             </form>
           </div>
         </Modal.Body>
