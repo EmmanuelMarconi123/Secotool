040e61ed81bcbb94deea7084e5c8d56b72207249
francroce <francocrocee7@gmail.com>
2023-08-24 19:03:29 -0300

    probando user

--- Frontend/secotool-app/src/App.jsx
+++ Frontend/secotool-app/src/App.jsx
@@ -2,26 +2,42 @@
 import "./rsuite.less";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import AuthRoutes from "./autenticaciones/AuthRoutes/AuthRoutes";
-import AppRoutes from "./autenticaciones/AppRoutes/AppRoutes";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/routes/Home";
+import Details from "./components/routes/Details";
+import NewProduct from "./components/routes/newProduct/NewProduct";
+import HomeAdmin from "./components/routes/homeAdmin/HomeAdmin";
+import AdminHeader from "./components/header/adminHeader/AdminHeader";
+import CrearCuenta from "./components/routes/crearCuenta/CrearCuenta";
+import LogIn from "./components/routes/logIn/LogIn";
+import Features from "./components/routes/features/Features";
 
 function App() {
   return (
     <body>
-    {window.location.pathname.includes('/admin') ? <AdminHeader /> : <Header />}
+      {window.location.pathname.includes("/admin") ? (
+        <AdminHeader />
+      ) : (
+        <Header />
+      )}
       <main className="spacing-grid">
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/crearCuenta" element={<CrearCuenta />}></Route>
-            <Route path="/login" element={<LogIn />}></Route>
-            <Route path="/product/:id" element={<Details />}></Route>
-            <Route path="/admin/home" element={<HomeAdmin />}></Route>
-            <Route path="/admin/newproduct" element={<NewProduct />}></Route>
-            <Route path="*" element={<div>Error 404</div>}></Route>
-          </Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/crearCuenta" element={<CrearCuenta />}></Route>
+          <Route path="/login" element={<LogIn />}></Route>
+          <Route path="/product/:id" element={<Details />}></Route>
+          <Route path="/admin/home" element={<HomeAdmin />}></Route>
+          <Route path="/admin/newproduct" element={<NewProduct />}></Route>
+          <Route
+            path="/admin/features"
+            element={<Features />}
+          ></Route>
+          <Route path="*" element={<div>Error 404</div>}></Route>
+        </Routes>
       </main>
-    <Footer className='footer'/>
+      <Footer className="footer" />
     </body>
   );
 }
