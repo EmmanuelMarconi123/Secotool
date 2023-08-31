4d1278598d118fe93ed4f6fa212fdc3d1f079502
Evelyn Tramontin <evelynat21@gmail.com>
2023-08-31 19:33:15 -0300

    se maqueto bloque calendario y politicas en detalle del producto. Se arreglo detalle de paginacion. Se agrego custom hook useFetch y loader en home

--- Frontend/secotool-app/src/components/routes/details/Details.jsx
+++ Frontend/secotool-app/src/components/routes/details/Details.jsx
@@ -4,11 +4,16 @@ import { useState } from "react";
 import { useEffect } from "react";
 import ListCaracteristicas from "../../list/ListCaracteristicas";
 import styles from "./Details.module.css";
+import { DateRangePicker } from "rsuite";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function Details() {
   const params = useParams();
   const [productD, setProductsD] = useState({});
-  console.log(params);
+  const isScreenSmall = useMediaQuery("(max-width: 767px)");
+  const [isSticky, setIsSticky] = useState(false);
+
+  //console.log(params);
 
   useEffect(() => {
     const fetchProductsD = async () => {
@@ -29,6 +34,25 @@ function Details() {
     };
 
     fetchProductsD();
+  }, []);
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
+    const bottomElement = document.querySelector(
+      `.${styles.boxInfoProductBottomEnd}`
+    );
+
+    if (bottomElement) {
+      const bottomElementOffset = bottomElement.offsetTop;
+      setIsSticky(scrollPosition > bottomElementOffset);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const productImagenes = [
@@ -53,6 +77,45 @@ function Details() {
  /* let images = productD.images;
   console.log(images);*/
 
+  const mockPoliticas = [
+    {
+      id: 1,
+      name: "Cancelación estricta",
+      description:
+        "Ante la cancelación del alquiler de un producto por parte del usuario, se descontará el 15% del valor del alquiler.  ",
+    },
+    {
+      id: 2,
+      name: "Garantía",
+      description:
+        "Se solicitará enviar foto del frente y dorso del DNI al momento de retirar el producto alquilado. ",
+    },
+    {
+      id: 3,
+      name: "Tarifas y Pagos",
+      description:
+        "Nuestra política de tarifas garantiza que los clientes conozcan el costo exacto del alquiler de la herramienta de construcción desde el principio. No habrá cargos ocultos ni sorpresas en la factura. El pago se realiza por adelantado y se aceptan varios métodos de pago, incluyendo tarjetas de crédito y transferencias bancarias",
+    },
+    {
+      id: 4,
+      name: "Depósito de Seguridad",
+      description:
+        "Para asegurar el uso responsable de nuestras herramientas, requerimos un depósito de seguridad antes del alquiler. Este depósito se reembolsará en su totalidad una vez que la herramienta sea devuelta en condiciones adecuadas. Cualquier daño o pérdida resultante del mal uso puede afectar la cantidad reembolsada.",
+    },
+    {
+      id: 5,
+      name: "Mantenimiento Post-Alquiler",
+      description:
+        "Esperamos que los clientes devuelvan la herramienta en condiciones limpias y funcionales. Si la herramienta se devuelve en mal estado o requiere una limpieza profunda, nos reservamos el derecho de aplicar cargos adicionales. Realizamos inspecciones exhaustivas después de cada alquiler para garantizar su calidad.",
+    },
+    {
+      id: 6,
+      name: "Seguro de Cobertura Amplia",
+      description:
+        "Ofrecemos un seguro opcional que cubre daños accidentales, pérdida y robo de la herramienta alquilada. Los clientes pueden optar por esta cobertura adicional para tener tranquilidad durante el uso. Es importante revisar los términos y condiciones del seguro para comprender plenamente qué situaciones están cubiertas.",
+    },
+  ];
+
   return (
     <div className="d-flex f-dir-colum">
       <Link to="/home">
@@ -61,31 +124,78 @@ function Details() {
         </button>
       </Link>
       <div className={styles.boxInfoProduct}>
-        <Carousel imagenes={productImagenes}></Carousel>
-        <h1 className="title-lg">{productD.name}</h1>
-        <div className="font-xl">
-          <span>$</span>
-          <span>{productD.price}</span>
+        <div>
+          <h1 className="title-lg">{productD.name}</h1>
+          <Carousel imagenes={productImagenes}></Carousel>
         </div>
-        <div className="pt-24">
-          <h4 className={styles.titleDetails + " font-regular mb-16"}>
-            Descripción
-          </h4>
-          <p className="font-sm">{productD.description}</p>
+        <div className={styles.boxInfoProductBottom}>
+          <div className={styles.boxInfoProductBottomStart}>
+            <div className="pt-24">
+              <h4 className={styles.titleDetails + " font-regular mb-16"}>
+                Descripción
+              </h4>
+              <p className="font-sm">{productD.description}</p>
+            </div>
+            <div className="">
+              <h4 className={styles.titleDetails + " font-regular mb-16"}>
+                Características
+              </h4>
+              {productD.productFeature ? (
+                <ListCaracteristicas
+                  caracteris={productD.productFeatures}
+                ></ListCaracteristicas>
+              ) : (
+                <p className="font-sm">El producto no posee características.</p>
+              )}
+            </div>
+          </div>
+          <div
+            className={
+              styles.boxInfoProductBottomEnd +
+              (isSticky ? " " + styles.stickyEnd : "")
+            }
+          >
+            <div className={styles.boxPrecioFechas}>
+              <div className={styles.boxCalendarTop}>
+                <div className={styles.boxTexts}>
+                  <span className={styles.titleSm}>Precio total</span>
+                  <div className={styles.textPrecio}>
+                    <span>$</span>
+                    <span>{productD.price}</span>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.boxCalendar}>
+                <span className={styles.titleSm}>Desde - Hasta</span>
+                {isScreenSmall ? (
+                  <DateRangePicker
+                    appearance="subtle"
+                    placeholder="Seleccione fechas"
+                    showOneCalendar
+                  />
+                ) : (
+                  <DateRangePicker
+                    appearance="subtle"
+                    placeholder="Seleccione fechas"
+                  />
+                )}
+              </div>
+            </div>
+            <button className="button-lg button-cta">Alquilar</button>
+          </div>
         </div>
-        <div className="">
-          <h4 className={styles.titleDetails + " font-regular mb-16"}>
-            Características
-          </h4>
-          {productD.productFeatures ? (
-             <ListCaracteristicas
-             caracteris={productD.productFeatures}
-           ></ListCaracteristicas>
-          ):(
-            <span>El producto no posee características.</span>
-          )}
+        <div className={styles.boxList}>
+          <ul className={styles.listPoliticas}>
+            {mockPoliticas.map((politica) => (
+              <li key={politica.id}>
+                <h4 className={styles.titleDetails + " font-regular mb-16"}>
+                  {politica.name}
+                </h4>
+                <p className="font-sm">{politica.description}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-        <button className="button-lg button-cta">Alquilar</button>
       </div>
     </div>
   );
