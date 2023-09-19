93ff85178ebb55cb99af26e02143d41cc7f5cb86
Evelyn Tramontin <evelynat21@gmail.com>
2023-09-18 21:38:32 -0300

    se aplico nuevos colores al header y bloque busqueda. Ademas se corrigio espaciado y responsividad en la seccion de filtros

--- Frontend/secotool-app/src/components/header/Header.jsx
+++ Frontend/secotool-app/src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import stylesHeader from "./Header.module.css";
 import NavBarHeader from "../navbar/NavBarHeader";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function Header() {
 
+  const isScreenSmall = useMediaQuery("(max-width: 768px)");
+
   return (
     <header className={stylesHeader.headerDefault}>
+      {isScreenSmall ? <img src="../src/assets/img/barra-mobile-tablet.png" /> :
+      <img src="../src/assets/img/barra-desktop.png" />}
         <NavBarHeader></NavBarHeader>
     </header>
   );
