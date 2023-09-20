8f1af1952341a1bdaed3d4d3f7c253f1a7e8eda0
darioplazaleon <darioplaza02@hotmail.com>
2023-09-20 12:53:18 -0300

    hola

--- Frontend/secotool-app/src/components/routes/home/Home.jsx
+++ Frontend/secotool-app/src/components/routes/home/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
         const fetchOptions = {
           headers: {
             Authorization: storedToken ? `Bearer ${storedToken}` : "", // Solo se incluye si hay un token
+            "Cache-Control": "no-cache"
           },
         };
 
